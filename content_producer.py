9c0a6ddf4856fa81d5cfcdd8a5cc0b3c209f920d
Benjamin Sobel <ben-developer@opayq.com>
2024-06-01 14:42:55 -0700

    clip generation

--- content_producer.py
+++ content_producer.py
@@ -3,16 +3,46 @@ import elevenlabs
 import assemblyai as aai
 
 from dotenv import load_dotenv
-from moviepy.editor import VideoFileClip, concatenate_videoclips, CompositeVideoClip, AudioFileClip, TextClip, ColorClip
+from moviepy.editor import (
+    VideoFileClip,
+    concatenate_videoclips,
+    CompositeVideoClip,
+    AudioFileClip,
+    TextClip,
+    ColorClip,
+)
 from moviepy.video.tools.subtitles import SubtitlesClip
 from elevenlabs import Voice, VoiceSettings
 from elevenlabs.client import ElevenLabs
 
+
 class ContentError(Exception):
     """Exception raised for errors in the ContentProducer class."""
+
     pass
 
-class ContentProducer: 
+
+class ContentData:
+    """A class for storing content data."""
+
+    def __init__(self, category, script, montage_url):
+        """Initializes the ContentData object.
+
+        Args:
+            category (str): The category of the content.
+            script (str): The script for the content.
+            montage_url (str): The URL of the montage video for the content.
+        """
+        self.category = category
+        self.script = script
+        self.montage_url = montage_url
+
+    def __str__(self):
+        """Returns a string representation of the ContentData object."""
+        return f"Category: {self.category}, Script: {self.script}, Montage URL: {self.montage_url}"
+
+
+class ContentProducer:
     """A class for producing content from provided content data."""
 
     def __init__(self):
@@ -37,103 +67,120 @@ class ContentProducer:
             self._create_video_short(content_data)
         except Exception as e:
             raise ContentError("Error creating content: " + str(e))
-        return 
+        return
 
     def _create_voice_over(self, content_data):
-            """Creates voice-over text-to-speech (TTS) audio files and subtitles.
+        """Creates voice-over text-to-speech (TTS) audio files and subtitles.
 
-            Args:
-                content_data (list): A list of ContentData objects containing script and montage URL.
-            """
-            for i, data in enumerate(content_data):
-                if data.category == 'tech_science':
-                    script = f"What's going on in tech today?\n\n" + data.script
-                else:
-                     script = f"What's going on in {data.category} today?\n\n" + data.script
+        Args:
+            content_data (list): A list of ContentData objects containing script and montage URL.
+        """
+        for i, data in enumerate(content_data):
+            if data.category == "tech_science":
+                script = f"What's going on in tech today?\n\n" + data.script
+            else:
+                script = f"What's going on in {data.category} today?\n\n" + data.script
 
-                audio_file_name = f'audio_clip_{i}.mp3' 
-                audio = self.client.generate(
-                    text=script, 
-                    voice=Voice(
-                        voice_id='yeNojDSkqjMLki231UWs',
-                        settings=VoiceSettings(stability=0.5, similarity_boost=0.75)
-                    ),
-                    model="eleven_turbo_v2"
-                )
-                elevenlabs.save(audio, audio_file_name)
+            audio_file_name = f"audio_clip_{i}.mp3"
+            audio = self.client.generate(
+                text=script,
+                voice=Voice(
+                    voice_id="yeNojDSkqjMLki231UWs",
+                    settings=VoiceSettings(stability=0.5, similarity_boost=0.75),
+                ),
+                model="eleven_turbo_v2",
+            )
+            elevenlabs.save(audio, audio_file_name)
 
-                transcript = aai.Transcriber().transcribe(audio_file_name)
-                print("Transcript: ", transcript.text)
-                subtitles = transcript.export_subtitles_srt(chars_per_caption=20)
+            transcript = aai.Transcriber().transcribe(audio_file_name)
+            print("Transcript: ", transcript.text)
+            subtitles = transcript.export_subtitles_srt(chars_per_caption=20)
 
-                with open(f"subtitles_{i}.srt", "w") as file:
-                    file.write(subtitles)
+            with open(f"subtitles_{i}.srt", "w") as file:
+                file.write(subtitles)
 
     def _create_video_short(self, content_data):
-            """Creates short video clips with text-to-speech (TTS) audio and subtitles.
+        """Creates short video clips with text-to-speech (TTS) audio and subtitles.
 
-            Args:
-                content_data (list): A list of ContentData objects containing script and montage URL.
-            """
-            for i, (data) in enumerate(content_data):
-                montage_url = data.montage_url
-                
-                filename = f'audio_clip_{i}.mp3' 
-                narration_audio = AudioFileClip(filename)
-                audio_duration = narration_audio.duration
+        Args:
+            content_data (list): A list of ContentData objects containing script and montage URL.
+        """
+        for i, (data) in enumerate(content_data):
+            montage_url = data.montage_url
 
-                def generator(txt, duration):
-                    """Generates a video clip with centered text over a clip mathcing the topic category.
+            filename = f"audio_clip_{i}.mp3"
+            narration_audio = AudioFileClip(filename)
+            audio_duration = narration_audio.duration
 
-                    Args:
-                        txt (str): The subtitle text.
-                        duration (float): Duration of the subtitle text display.
+            def generator(txt, duration):
+                """Generates a video clip with centered text over a clip mathcing the topic category.
 
-                    Returns:
-                        CompositeVideoClip: A video clip with the subtitle text and background.
-                    """
-                    font_size = 40
-                    dummy_clip = TextClip(txt, font="Arial-Bold", fontsize=font_size) # Create a dummy TextClip to measure text size
-                    text_width, text_height = dummy_clip.size
-                    
-                    padding_x = 20
-                    padding_y = 10
-                    bg_width = text_width + padding_x * 2
-                    bg_height = text_height + padding_y * 2
+                Args:
+                    txt (str): The subtitle text.
+                    duration (float): Duration of the subtitle text display.
 
-                    return CompositeVideoClip([
-                        ColorClip((bg_width, bg_height), color=(0, 0, 0)).set_opacity(0.5).set_duration(duration), 
+                Returns:
+                    CompositeVideoClip: A video clip with the subtitle text and background.
+                """
+                font_size = 40
+                dummy_clip = TextClip(
+                    txt, font="Arial-Bold", fontsize=font_size
+                )  # Create a dummy TextClip to measure text size
+                text_width, text_height = dummy_clip.size
+
+                padding_x = 20
+                padding_y = 10
+                bg_width = text_width + padding_x * 2
+                bg_height = text_height + padding_y * 2
+
+                return CompositeVideoClip(
+                    [
+                        ColorClip((bg_width, bg_height), color=(0, 0, 0))
+                        .set_opacity(0.5)
+                        .set_duration(duration),
                         TextClip(
                             txt,
                             font="Arial-Bold",
                             fontsize=font_size,
-                            color='white',
-                            align='center',
-                            size=(bg_width, None),  # Set width, let height adjust dynamically
-                        ).set_position(('center', 'center')).set_duration(duration)
-                    ])
+                            color="white",
+                            align="center",
+                            size=(
+                                bg_width,
+                                None,
+                            ),  # Set width, let height adjust dynamically
+                        )
+                        .set_position(("center", "center"))
+                        .set_duration(duration),
+                    ]
+                )
 
-                subtitles_clip = SubtitlesClip(f"subtitles_{i}.srt", lambda txt, duration=audio_duration: generator(txt, duration))
+            subtitles_clip = SubtitlesClip(
+                f"subtitles_{i}.srt",
+                lambda txt, duration=audio_duration: generator(txt, duration),
+            )
 
-                video_clip = VideoFileClip(montage_url)
-                video_duration = video_clip.duration
+            video_clip = VideoFileClip(montage_url)
+            video_duration = video_clip.duration
 
-                if video_duration < audio_duration:
-                    repeated_video_clip = video_clip.loop(duration=audio_duration)
-                    adjusted_video_clip = repeated_video_clip
-                elif video_duration > audio_duration:
-                    adjusted_video_clip = video_clip.subclip(0, audio_duration)
-                
-                final_with_subtitles = adjusted_video_clip.set_audio(narration_audio).set_duration(adjusted_video_clip.duration)
-                final_with_subtitles = CompositeVideoClip([
-                    final_with_subtitles,
-                    subtitles_clip.set_pos("center")
-                ])
+            if video_duration < audio_duration:
+                repeated_video_clip = video_clip.loop(duration=audio_duration)
+                adjusted_video_clip = repeated_video_clip
+            elif video_duration > audio_duration:
+                adjusted_video_clip = video_clip.subclip(0, audio_duration)
 
-                outro_audio = AudioFileClip("outro_audio.mp3")   
-                outro_clip = VideoFileClip("outro_clip.mp4").set_audio(outro_audio)
+            final_with_subtitles = adjusted_video_clip.set_audio(
+                narration_audio
+            ).set_duration(adjusted_video_clip.duration)
+            final_with_subtitles = CompositeVideoClip(
+                [final_with_subtitles, subtitles_clip.set_pos("center")]
+            )
 
-                clip_array=[final_with_subtitles, outro_clip]
+            # outro_audio = AudioFileClip("outro_audio.mp3")
+            # outro_clip = VideoFileClip("outro_clip.mp4").set_audio(outro_audio)
 
-                final_short = concatenate_videoclips(clip_array)
-                final_short.write_videofile(f"final_clip_{i}.mp4", fps=24)
+            # clip_array = [final_with_subtitles, outro_clip]
+
+            # final_short = concatenate_videoclips(clip_array)
+
+            final_short = final_with_subtitles
+            final_short.write_videofile(f"out/final_clip_{i}.mp4", fps=24)
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, HttpUrl, validator
 from fastapi.middleware.cors import CORSMiddleware
 
+from content_producer import ContentData, ContentProducer
 from generate_vid.gen import generate_video_internal
 from generate_vid.script import gen_script_from_text
+import uuid
+
+content_producer = ContentProducer()
 
 app = FastAPI()
 
@@ -35,6 +40,17 @@ async def generate_video(vid: VidGenerationRequest):
         # Generate video and return absolute directory
         vid_dir = generate_video_internal(vid.text)
         script = gen_script_from_text(vid.text)
-        return VidGenerationResponse(video_url=vid_dir, script=script)
+        # creates the video, written to final_clip_0.mp4 or final_clip_1.mp4
+        content_producer.invoke([ContentData("tech_science", script, vid_dir)])
+        # create the url to the video
+        out_dir1 = "out/final_clip_0.mp4"
+        out_dir2 = "out/final_clip_1.mp4"
+        vid_dir = out_dir1 if os.path.exists(out_dir1) else out_dir2
+        # move the video to the static directory (outdata) and rename with unique name
+        unique_name = uuid.uuid4().hex
+        vid_dir2 = f"outdata/{unique_name}.mp4"
+        # move the video to the outdata directory
+        os.rename(vid_dir, vid_dir2)
+        return VidGenerationResponse(video_url=vid_dir2, script=script)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
